5af4ad61f8668e4ca230a44c4ccb1f9680996d15
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-22 17:14:38 -0600

    Data loading and displaying from OMDB.

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -1,9 +1,76 @@
-app.controller('HomeController', ['$scope', '$http', '$route', function($scope, $http, $route) {
+app.controller('HomeController', ['$scope', '$http', '$route', '$location', function($scope, $http, $route, $location) {
   console.log("Home controller.");
   $http.get('https://pure-wave-92261.herokuapp.com/movies/movies/').then(function(response) { // INDEX
     $scope.movies = response.data;
-    console.log($scope.movies);
   }, function(response) {
     console.log("Error, no data returned.");
   });
+
+  $scope.getLocation = function(val) {
+    return $http.get('//www.omdbapi.com/?s=' + val)
+    .then(function(response){
+      return response.data.Search.map(function(item){
+        return item.Title;
+      });
+    });
+  };
+
+  $scope.onSelect = function ($item) {
+    console.log("Selected!");
+    console.log($item);
+    return $http.get('//www.omdbapi.com/?t=' + $item)
+    .then(function(response){
+      var movie = {
+        movieActors: response.data.Actors,
+        movieAwards: response.data.Awards,
+        movieCountry: response.data.Country,
+        movieDirector: response.data.Director,
+        movieGenre: response.data.Genre,
+        movieLanguage: response.data.Language,
+        movieMetascore: response.data.Metascore,
+        moviePlot: response.data.Plot,
+        moviePoster: response.data.Poster,
+        movieRated: response.data.Rated,
+        movieRuntime: response.data.Runtime,
+        movieTitle: response.data.Title,
+        movieWriter: response.data.Writer,
+        movieYear: response.data.Year,
+        movieImdbID: response.data.imdbID,
+        movieImdbRating: response.data.imdbRating,
+        movieImdbVotes: response.data.imdbVotes,
+        movieLikes: 0
+      };
+      $http.post('https://pure-wave-92261.herokuapp.com/movies/movies/', movie).then(function(response) { // NEW
+        console.log("Movie added.");
+        console.log(response.data._id);
+
+        $scope.movie.movieActors =  movie.movieActors;
+        $scope.movie.movieAwards =  movie.movieAwards;
+        $scope.movie.movieCountry = movie.movieCountry;
+        $scope.movie.movieDirector = movie.movieDirector;
+        $scope.movie.movieGenre = movie.movieGenre;
+        $scope.movie.movieLanguage = movie.movieLanguage;
+        $scope.movie.movieMetascore = movie.movieMetascore;
+        $scope.movie.moviePlot = movie.moviePlot;
+        $scope.movie.moviePoster = movie.moviePoster;
+        $scope.movie.movieRated = movie.movieRated;
+        $scope.movie.movieRuntime = movie.movieRuntime;
+        $scope.movie.movieTitle = movie.movieTitle;
+        $scope.movie.movieWriter = movie.movieWriter;
+        $scope.movie.movieYear = movie.movieYear;
+        $scope.movie.movieImdbID = movie.movieImdbID;
+        $scope.movie.movieImdbRating = movie.movieImdbRating;
+        $scope.movie.movieImdbVotes = movie.movieImdbVotes;
+        $scope.movie.movieLikes = 0
+
+        console.log($scope.movie);
+        console.log(movie);
+
+        // $location.path( "/movies/" + response.data._id );
+      }, function(response) {
+        console.log("Error, no movie added.");
+      });
+    });
+  };
+
 }]);
--- public/javascript/controllers/NewController.js
+++ public/javascript/controllers/NewController.js
@@ -1,31 +1,9 @@
 app.controller('NewController', ['$scope', '$http', '$location', function($scope, $http, $location) {
   console.log("New controller.");
-  // $scope.addMovie = function(movie){
-  //   console.log($scope.movie);
-  //   var movie = {
-  //     movieName:  $scope.movie.movieName,
-  //     moviePerson: $scope.movie.moviePerson,
-  //     movieYear: $scope.movie.movieYear,
-  //     movieSummary: $scope.movie.movieSummary,
-  //     movieTrivia: $scope.movie.movieTrivia,
-  //     moviePoster: $scope.movie.moviePoster,
-  //     movieRating: $scope.movie.movieRating,
-  //     movieLikes: 0
-  //   };
-  //   $http.post('https://pure-wave-92261.herokuapp.com/movies/movies/', movie).then(function(response) { // NEW
-  //     console.log("Movie added.");
-  //     console.log(movie);
-  //     $location.path( "/movies/" );
-  //   }, function(response) {
-  //     console.log("Error, no movie added.");
-  //   });
-  // };
 
   $scope.addMovie = function(movie){
-    console.log($scope.movie);
     return $http.get('//www.omdbapi.com/?t=' + $scope.movie.movieName)
     .then(function(response){
-      console.log(response);
       var movie = {
         movieName:  response.data.Title,
         moviePerson: response.data.Director,
@@ -40,7 +18,6 @@ app.controller('NewController', ['$scope', '$http', '$location', function($scope
       };
       $http.post('https://pure-wave-92261.herokuapp.com/movies/movies/', movie).then(function(response) { // NEW
         console.log("Movie added.");
-        console.log(movie);
         $location.path( "/movies/" );
       }, function(response) {
         console.log("Error, no movie added.");
@@ -48,35 +25,69 @@ app.controller('NewController', ['$scope', '$http', '$location', function($scope
     });
   };
 
-  // $http.post('https://pure-wave-92261.herokuapp.com/movies/movies/', movie).then(function(response) { // NEW
-  //   console.log("Movie added.");
-  //   console.log(movie);
-  //   $location.path( "/movies/" );
-  // }, function(response) {
-  //   console.log("Error, no movie added.");
-  // });
-  // };
-
-  // $scope.getLocation = function(val) {
-  //   return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
-  //     params: {
-  //       address: val,
-  //       sensor: false
-  //     }
-  //   }).then(function(response){
-  //     console.log(response);
-  //     return response.data.results.map(function(item){
-  //       console.log(item)
-  //       return item.formatted_address;
-  //     });
-  //   });
-  // };
-
   $scope.getLocation = function(val) {
     return $http.get('//www.omdbapi.com/?s=' + val)
     .then(function(response){
       return response.data.Search.map(function(item){
         return item.Title;
+      });
+    });
+  };
+
+  $scope.onSelect = function ($item) {
+    console.log("Selected!");
+    console.log($item);
+    return $http.get('//www.omdbapi.com/?t=' + $item)
+    .then(function(response){
+      var movie = {
+        movieActors: response.data.Actors,
+        movieAwards: response.data.Awards,
+        movieCountry: response.data.Country,
+        movieDirector: response.data.Director,
+        movieGenre: response.data.Genre,
+        movieLanguage: response.data.Language,
+        movieMetascore: response.data.Metascore,
+        moviePlot: response.data.Plot,
+        moviePoster: response.data.Poster,
+        movieRated: response.data.Rated,
+        movieRuntime: response.data.Runtime,
+        movieTitle: response.data.Title,
+        movieWriter: response.data.Writer,
+        movieYear: response.data.Year,
+        movieImdbID: response.data.imdbID,
+        movieImdbRating: response.data.imdbRating,
+        movieImdbVotes: response.data.imdbVotes,
+        movieLikes: 0
+      };
+      $http.post('https://pure-wave-92261.herokuapp.com/movies/movies/', movie).then(function(response) { // NEW
+        console.log("Movie added.");
+        console.log(response.data._id);
+
+        $scope.movie.movieActors =  movie.movieActors;
+        $scope.movie.movieAwards =  movie.movieAwards;
+        $scope.movie.movieCountry = movie.movieCountry;
+        $scope.movie.movieDirector = movie.movieDirector;
+        $scope.movie.movieGenre = movie.movieGenre;
+        $scope.movie.movieLanguage = movie.movieLanguage;
+        $scope.movie.movieMetascore = movie.movieMetascore;
+        $scope.movie.moviePlot = movie.moviePlot;
+        $scope.movie.moviePoster = movie.moviePoster;
+        $scope.movie.movieRated = movie.movieRated;
+        $scope.movie.movieRuntime = movie.movieRuntime;
+        $scope.movie.movieTitle = movie.movieTitle;
+        $scope.movie.movieWriter = movie.movieWriter;
+        $scope.movie.movieYear = movie.movieYear;
+        $scope.movie.movieImdbID = movie.movieImdbID;
+        $scope.movie.movieImdbRating = movie.movieImdbRating;
+        $scope.movie.movieImdbVotes = movie.movieImdbVotes;
+        $scope.movie.movieLikes = 0
+
+        console.log($scope.movie);
+        console.log(movie);
+
+        $location.path( "/movies/" + response.data._id );
+      }, function(response) {
+        console.log("Error, no movie added.");
       });
     });
   };
