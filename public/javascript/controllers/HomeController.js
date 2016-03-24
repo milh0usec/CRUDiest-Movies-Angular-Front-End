07105e3e888a2043f356160ac786f6c96ec3272a
Thomas David Kehoe <kehoe@casafuturatech.com>
2016-03-24 14:33:07 -0600

    orderBy six choices working.

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -2,6 +2,9 @@ app.controller('HomeController', ['$scope', '$http', '$route', '$location', func
   console.log("Home controller.");
   $http.get('https://pure-wave-92261.herokuapp.com/movies/movies/').then(function(response) { // INDEX
     $scope.movies = response.data;
+    $scope.order = '_id';
+    $scope.reverse = true;
+    console.log($scope.movies);
   }, function(response) {
     console.log("Error, no data returned.");
   });
